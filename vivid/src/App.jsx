88f052bb6c48bf7e5b0faf1efa75f3e2a0f2690b
Vinay Damarasing <satyavinay0527@gmail.com>
2025-04-19 07:29:05 +0530

    deleted prev community thing and started again . created community cards necessary files. created a data file .

--- vivid/src/App.jsx
+++ vivid/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <NavBar />
       <MainSection />
       <PeopleCardsContainer />
-      <CommunityCardsSection />
       <Content />
     </div>
   );
---
+++ vivid/src/Components/CommunityCards/CommunityCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CommunityCards() {
+  return (
+    <div>CommunityCards</div>
+  )
+}
+
+export default CommunityCards
